63eb7207a97d51c95881c54eae37212a5bf4773e
Praba Rai <94899684+prabarai@users.noreply.github.com>
2022-12-20 22:45:25 +0600

    feat(main): tailwind implementation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "build/index.html"],
+
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
